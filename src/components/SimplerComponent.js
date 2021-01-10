8e46616dae107ea86caed051ca4029525facbb53
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-01-09 21:23:14 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
-// Code SimplerComponent Here
+import React from 'react'
+
+const defaultText = "I am just happy"
+
+const SimplerComponent = (props) => 
+    <div onClick={props.handleClick}>
+        {defaultText}
+    </div>
+
+export default SimplerComponent
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+// let onClick = () => {
+//   return "not"
+// }
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
